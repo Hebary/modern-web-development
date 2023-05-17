b0986d32e15040484b89952c803c8dbe19ee2e37
shakharho <berdaddario@gmail.com>
2023-05-17 08:23:13 -0300

    feat:fix theme toggle icon issue

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,23 +1,43 @@
 'use client'
-import { IconButton } from '@mui/material'
-import { DarkMode, LightMode } from '@mui/icons-material'
-import { grey } from '@mui/material/colors'
 import { useTheme } from 'next-themes'
+import { IconButton } from '@mui/material'
+import { DarkModeOutlined, LightMode } from '@mui/icons-material'
+import { grey } from '@mui/material/colors'
+import { useEffect, useState } from 'react'
 
 const ThemeToggle = () => {
   const { setTheme, theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setTheme('dark')
+    setMounted(true)
+  },[])
+
   const handleTheme = () => {
     if (theme === 'dark') {
       setTheme('light')
-    } else {
+    } else if(theme==='light'){
       setTheme('dark')
     }
   }
 
   return (
-    <IconButton onClick={ handleTheme } >
-      {theme === 'dark' ? ( <LightMode sx={{ color: grey[600] }} /> ) : ( <DarkMode className='text-slate-500' /> ) }
-    </IconButton>
+  <>
+    { theme === 'dark' && mounted ? (
+      
+      <IconButton onClick={ handleTheme } >
+        <LightMode sx={{ color: grey[600] }} /> 
+      </IconButton>
+      
+      ) : (
+      
+      <IconButton onClick={ handleTheme } >
+         <DarkModeOutlined sx={{color: grey[600] }} /> 
+      </IconButton>
+    
+    )}
+</>
   )
 }
 export default ThemeToggle
